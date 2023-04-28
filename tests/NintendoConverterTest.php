eea14725c45b0cc57fbbfd40e9a8eb505b8fa389
Nick Been <66799549+nickbeen@users.noreply.github.com>
2023-04-28 18:29:23 +0200

    Add convenient fromBlocks and fromMegabytes methods

--- tests/NintendoConverterTest.php
+++ tests/NintendoConverterTest.php
@@ -15,8 +15,8 @@ class NintendoConverterTest extends TestCase
      */
     public function it_can_convert_blocks_to_megabytes()
     {
-        $blocks = new NintendoConverter(blocks: 8);
-        $megabytes = $blocks->toMegabytes();
+        $nC = new NintendoConverter();
+        $megabytes = $nC->fromBlocks(8)->toMegabytes();
 
         $this->assertEquals(1, $megabytes);
     }
@@ -27,8 +27,8 @@ class NintendoConverterTest extends TestCase
      */
     public function it_can_convert_megabytes_to_blocks()
     {
-        $megabytes = new NintendoConverter(megabytes: 1);
-        $blocks = $megabytes->toBlocks();
+        $nC = new NintendoConverter();
+        $blocks = $nC->fromMegabytes(1)->toBlocks();
 
         $this->assertEquals(8, $blocks);
     }
